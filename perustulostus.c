561cc38266abbd3bd37eea04979d2352cafb61d4
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2023-09-27 12:16:43 +0300

    created

---
+++ perustulostus.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    unsigned long n = 4000;
+    unsigned short laskuri = 0;
+    char luokka = 'C';
+    float keskiarvo = 0.0;
+
+    printf("%li\n", n);
+    printf("%hu\n", laskuri);
+    printf("%c\n", luokka);
+    printf("%f\n", keskiarvo);
+
+    return 0;
+}
