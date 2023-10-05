988a2118c4252dfbeb8f32fdb6c522ed21cc8e9f
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2023-10-05 16:20:29 +0300

    hruheosufhseuifseiuf

--- ristinolla.c
+++ ristinolla.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <inttypes.h>
+#define RISTI 1
+#define NOLLA 2
+
 
 int8_t tictactoe_check(int8_t *gameboard, int win_len);
+int8_t count_wins(int8_t *gameboard, int win_len, int8_t player);
 
 int main(void)
 {
@@ -25,10 +29,30 @@ int8_t tictactoe_check(int8_t *gameboard, int win_len)
     int8_t gameboard_2d[10][10];
     for (int8_t i = 0; i < 10; i++)
     {
-        for (int8_t j = 0; j < 10; j++){
-            gameboard_2d[i][j] = gameboard[10*i+j];
+        for (int8_t j = 0; j < 10; j++)
+        {
+            gameboard_2d[i][j] = gameboard[10 * i + j];
         }
     }
 
+    int8_t risti_wins = count_wins(gameboard_2d, win_len, RISTI);
+    int8_t nolla_wins = count_wins(gameboard_2d, win_len, NOLLA);
+
     return 0;
+}
+
+int8_t count_wins(int8_t *gameboard_2d, int win_len, int8_t player){
+    for (int8_t i = 0; i < 10; i++)
+    {
+        for (int8_t j = 0; j < 10; j++)
+        {
+            switch(gameboard_2d[i][j]){
+                case player:
+                    check_win_horizontally(int8_t *gameboard_2d, player);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
