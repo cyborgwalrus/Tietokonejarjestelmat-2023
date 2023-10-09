bbd21a88af68bf3cbc1ad58fc6ae3e366eb5b048
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2023-10-09 18:20:11 +0300

    adfghgggfgddgddfg

--- ristinolla.c
+++ ristinolla.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <inttypes.h>
+#include <stdbool.h>
 
 int8_t tictactoe_check(int8_t *gameboard, int win_len);
+int8_t gameboard_to_2d(int8_t *gameboard);
+
+bool check_vertical(int8_t *gameboard_2d, int win_len);
+bool check_horizontal(int8_t *gameboard_2d, int win_len);
+bool check_top_left_to_lower_right(int8_t *gameboard_2d, int win_len);
+bool check_top_right_to_lower_left(int8_t *gameboard_2d, int win_len);
 
 int main(void)
 {
@@ -20,8 +27,7 @@ int main(void)
     return 0;
 }
 
-int8_t tictactoe_check(int8_t *gameboard, int win_len)
-{
+int8_t gameboard_to_2d(int8_t *gameboard){
     int8_t gameboard_2d[10][10];
     for (int8_t i = 0; i < 10; i++)
     {
@@ -29,6 +35,16 @@ int8_t tictactoe_check(int8_t *gameboard, int win_len)
             gameboard_2d[i][j] = gameboard[10*i+j];
         }
     }
+    return &gameboard_2d;
+}
 
+bool check_vertical(int8_t *gameboard_2d, int win_len, int8_t player);
+bool check_horizontal(int8_t *gameboard_2d, int win_len, int8_t player);
+bool check_top_left_to_lower_right(int8_t *gameboard_2d, int win_len, int8_t player);
+bool check_top_right_to_lower_left(int8_t *gameboard_2d, int win_len, int8_t player);
+
+int8_t tictactoe_check(int8_t *gameboard, int win_len)
+{
+    int8_t gameboard_2d[10][10] = gameboard_to_2d(gameboard);
     return 0;
 }
