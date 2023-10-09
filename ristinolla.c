486611e78f5a8d1344a8a2e40a32b86aee1578b8
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2023-10-09 20:43:46 +0300

    Program completed

--- ristinolla.c
+++ ristinolla.c
@@ -10,38 +10,115 @@
 int8_t tictactoe_check(int8_t* gameboard, int win_len);
 
 bool check_horizontal(int8_t* gameboard, int win_len, int8_t player);
+bool check_vertical(int8_t* gameboard, int win_len, int8_t player);
+bool check_top_left_to_lower_right(int8_t* gameboard, int win_len, int8_t player);
+bool check_top_right_to_lower_left(int8_t* gameboard, int win_len, int8_t player);
 
 int main(void)
 {
     int8_t gameboard[100] = {
-        0,0,0,0,0,1,1,1,1,1,
-        0,1,0,0,0,0,0,0,0,0,
-        0,0,1,0,0,0,0,0,0,0,
+        1,0,0,0,0,1,1,1,1,1,
+        0,1,0,0,0,0,0,0,1,0,
+        0,0,1,0,0,0,0,1,0,0,
         0,0,0,1,1,1,1,1,0,0,
-        0,0,0,0,1,0,1,0,0,0,
-        0,0,0,0,0,1,1,0,0,0,
-        0,0,0,0,0,0,1,0,0,0,
-        0,0,0,0,0,0,1,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,1,1,1,0,0,0,
+        0,0,0,0,1,1,1,0,0,0,
+        0,0,0,1,0,0,1,0,0,0,
+        0,0,1,0,0,0,1,1,0,0,
+        0,1,0,0,0,0,0,0,1,0,
+        1,0,0,0,0,0,0,0,0,0,
     };
 
 
-    return tictactoe_check(gameboard, 5);
+    return tictactoe_check(gameboard, 10);
 }
 
 bool check_horizontal(int8_t* gameboard, int win_len, int8_t player)
 {
     for (int8_t i = 0; i < BOARD_HEIGHT; i++)
     {
-        for (int8_t j = 0; j < BOARD_WIDTH - win_len; j++)
+        for (int8_t j = 0; j <= BOARD_WIDTH - win_len; j++)
         {
             int8_t line_length = 0;
-            if (gameboard[10*i+j] == player)
+            if (gameboard[10 * i + j] == player)
             {
                 for (int8_t k = 0; k < win_len; k++)
                 {
-                    if (gameboard[10*i+j+k] == player)
+                    if (gameboard[10 * i + j + k] == player)
+                        line_length++;
+                }
+            }
+            if (line_length >= win_len)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool check_vertical(int8_t* gameboard, int win_len, int8_t player)
+{
+    for (int8_t i = 0; i <= BOARD_HEIGHT - win_len; i++)
+    {
+        for (int8_t j = 0; j <= BOARD_WIDTH; j++)
+        {
+            int8_t line_length = 0;
+            if (gameboard[10 * i + j] == player)
+            {
+                for (int8_t k = 0; k < win_len; k++)
+                {
+                    if (gameboard[10 * (i + k) + j] == player)
+                        line_length++;
+                }
+            }
+            if (line_length >= win_len)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool check_top_left_to_lower_right(int8_t* gameboard, int win_len, int8_t player)
+{
+    for (int8_t i = 0; i <= BOARD_HEIGHT - win_len; i++)
+    {
+        for (int8_t j = 0; j <= BOARD_WIDTH - win_len; j++)
+        {
+            int8_t line_length = 0;
+            if (gameboard[10 * i + j] == player)
+            {
+                for (int8_t k = 0; k < win_len; k++)
+                {
+                    if (gameboard[10 * (i + k) + (j + k)] == player)
+                        line_length++;
+                }
+            }
+            if (line_length >= win_len)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool check_top_right_to_lower_left(int8_t* gameboard, int win_len, int8_t player)
+{
+    for (int8_t i = 0; i < 10; i++)
+    {
+        for (int8_t j = 9; j >= 0; j--)
+        {
+            if (i + win_len > 10  || j + 1 - win_len < 0) break;
+
+            int8_t line_length = 0;
+            if (gameboard[10 * i + j] == player)
+            {
+                for (int8_t k = 0; k < win_len; k++)
+                {
+                    if (gameboard[10 * (i + k) + (j - k)] == player)
                         line_length++;
                 }
             }
@@ -56,8 +133,17 @@ bool check_horizontal(int8_t* gameboard, int win_len, int8_t player)
 
 int8_t tictactoe_check(int8_t* gameboard, int win_len)
 {
-    bool risti_wins = check_horizontal(gameboard, win_len, RISTI);
-    bool nolla_wins = check_horizontal(gameboard, win_len, NOLLA);
+    int8_t risti_wins = check_horizontal(gameboard, win_len, RISTI);
+    int8_t nolla_wins = check_horizontal(gameboard, win_len, NOLLA);
+    risti_wins += check_vertical(gameboard, win_len, RISTI);
+    nolla_wins += check_vertical(gameboard, win_len, NOLLA);
+    risti_wins += check_top_left_to_lower_right(gameboard, win_len, RISTI);
+    nolla_wins += check_top_left_to_lower_right(gameboard, win_len, NOLLA);
+    risti_wins += check_top_right_to_lower_left(gameboard, win_len, RISTI);
+    nolla_wins += check_top_right_to_lower_left(gameboard, win_len, NOLLA);
+
+
+
 
     if (risti_wins && nolla_wins) return 0;
     if (risti_wins) return 1;
