9ca5f46bc06068e7601062a5deba370af37de47d
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2023-10-09 19:43:43 +0300

    gdfhjiogfdjisdfgåhipgzfsdå

--- ristinolla.c
+++ ristinolla.c
@@ -4,34 +4,38 @@
 
 #define RISTI 1
 #define NOLLA 2
+#define BOARD_HEIGHT 10
+#define BOARD_WIDTH 10
 
-int8_t tictactoe_check(int8_t *gameboard, int win_len);
-int8_t gameboard_to_2d(int8_t *gameboard);
+int8_t tictactoe_check(int8_t* gameboard, int win_len);
+void initialize_2d_gameboard(int8_t* gameboard, int8_t gameboard_2d[10][10]);
 
-bool check_vertical(int8_t *gameboard_2d, int win_len);
-bool check_horizontal(int8_t *gameboard_2d, int win_len);
-bool check_top_left_to_lower_right(int8_t *gameboard_2d, int win_len);
-bool check_top_right_to_lower_left(int8_t *gameboard_2d, int win_len);
+bool check_vertical(int8_t gameboard_2d[10][10], int win_len, int8_t player);
+bool check_horizontal(int8_t* gameboard_2d, int win_len, int8_t player);
+bool check_top_left_to_lower_right(int8_t* gameboard_2d, int win_len, int8_t player);
+bool check_top_right_to_lower_left(int8_t* gameboard_2d, int win_len, int8_t player);
 
 int main(void)
 {
-    int8_t gameboard[100] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                             1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                             1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                             1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                             1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                             1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                             1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                             1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                             1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                             1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    tictactoe_check(gameboard, 3);
+    int8_t gameboard[100] = {
+        0,0,0,0,0,1,1,1,1,1,
+        0,1,0,0,0,0,0,0,0,0,
+        0,0,1,0,0,0,0,0,0,0,
+        0,0,0,1,1,1,1,1,0,0,
+        0,0,0,0,1,0,1,0,0,0,
+        0,0,0,0,0,1,1,0,0,0,
+        0,0,0,0,0,0,1,0,0,0,
+        0,0,0,0,0,0,1,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,
+    };
+    int8_t wins = tictactoe_check(gameboard, 5);
 
-    return 0;
+    return wins;
 }
 
-int8_t gameboard_to_2d(int8_t *gameboard){
-    int8_t gameboard_2d[10][10];
+void initialize_2d_gameboard(int8_t* gameboard, int8_t gameboard_2d[10][10])
+{
     for (int8_t i = 0; i < 10; i++)
     {
         for (int8_t j = 0; j < 10; j++)
@@ -39,16 +43,43 @@ int8_t gameboard_to_2d(int8_t *gameboard){
             gameboard_2d[i][j] = gameboard[10 * i + j];
         }
     }
-    return &gameboard_2d;
 }
 
-bool check_vertical(int8_t *gameboard_2d, int win_len, int8_t player);
-bool check_horizontal(int8_t *gameboard_2d, int win_len, int8_t player);
-bool check_top_left_to_lower_right(int8_t *gameboard_2d, int win_len, int8_t player);
-bool check_top_right_to_lower_left(int8_t *gameboard_2d, int win_len, int8_t player);
-
-int8_t tictactoe_check(int8_t *gameboard, int win_len)
+bool check_vertical(int8_t gameboard_2d[10][10], int win_len, int8_t player)
 {
-    int8_t gameboard_2d[10][10] = gameboard_to_2d(gameboard);
-    return 0;
+    for (int8_t i = 0; i < BOARD_HEIGHT; i++)
+    {
+        for (int8_t j = 0; j < BOARD_WIDTH - win_len; j++)
+        {
+            int8_t line_length = 0;
+            if (gameboard_2d[j][i] == player)
+            {
+                for (int8_t k = 0; k < win_len; k++)
+                {
+                    if (gameboard_2d[j + k][i] == player)
+                        line_length++;
+                }
+            }
+            if (line_length >= win_len)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+bool check_horizontal(int8_t* gameboard_2d, int win_len, int8_t player);
+bool check_top_left_to_lower_right(int8_t* gameboard_2d, int win_len, int8_t player);
+bool check_top_right_to_lower_left(int8_t* gameboard_2d, int win_len, int8_t player);
+
+int8_t tictactoe_check(int8_t* gameboard, int win_len)
+{
+    int8_t gameboard_2d[10][10] = { 0 };
+    initialize_2d_gameboard(gameboard, gameboard_2d);
+    bool risti_wins = check_vertical(gameboard_2d, win_len, RISTI);
+    bool nolla_wins = check_vertical(gameboard_2d, win_len, NOLLA);
+
+    if (risti_wins && nolla_wins) return 0;
+    if (risti_wins) return 1;
+    if (nolla_wins) return 2;
 }
