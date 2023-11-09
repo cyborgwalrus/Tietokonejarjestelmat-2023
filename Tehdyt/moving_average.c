2596df5ad77594d54fb26b698785ea392cd8c895
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2023-11-09 14:54:30 +0200

    fix

--- Tehdyt/moving_average.c
+++ Tehdyt/moving_average.c
@@ -2,12 +2,12 @@
 #include <inttypes.h>
 #include <string.h>
 
-void movavg(float *array, uint8_t array_size, uint8_t window_size);
+void moving_average(float *array, uint8_t array_size, uint8_t window_size);
 
 int main(void)
 {
     float testArray[] = {11.67, 12.91, 15.81, 17.21, 13.39, 14.34, 12.72, 17.47, 16.66, 16.05, 14.42, 18.41, 19.02, 18.82, 18.43, 11.61, 17.5, 16.66, 14.02, 15.33, 16.88, 12.58, 12.8, 13.64, 18.99, 19.44};
-    movavg(testArray, 26, 3);
+    moving_average(testArray, 26, 3);
 
     return 0;
 }
