cb6e5af44becad465c84087a28a41482b431bc69
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2023-12-14 19:42:44 +0200

    Muutin logiikkaa

--- Assembly/tehokertolasku_proto.py
+++ Assembly/tehokertolasku_proto.py
@@ -1,29 +1,37 @@
 def kertolasku(num1, num2):
     # initialize variables
-    bitmask = 0b1
+    bitmask = 1
     output = 0
+    bitshifted_num1 = num1
     remaining = num2
-    bitshift_counter = 0
     while(remaining != 0):
-        # read bit using bitmask
+        # read a bit using bitmask
         variable = num2
         variable = bitmask & variable
         if(variable != 0):
-            # bitshift num1 and add it to output
-            bitshift_input = num1
-            bitshift_output = bitshift_input << bitshift_counter
-            output = bitshift_output + output
-            remaining = bitmask ^ remaining
-        
-        # increment bitmask and counter
-        bitshift_counter += 1
+            # add bitshifted_num1 to output
+            output = bitshifted_num1 + output
+    
+    # process remaining
+        # invert bitmask
+        variable = 0
+        bitmask = variable - bitmask
+        # remove processed bit from remaining
+        remaining = bitmask & remaining
+        # invert bitmask again
+        bitmask = variable - bitmask
+    # bitshift bitmask and bitshifted_num1
+        # increment bitmask
         bitmask = bitmask + bitmask
-
-        print(f"num1: {num1}, num2:{num2}")
-
+        # bitshift num1
+        bitshifted_num1 = bitshifted_num1 + bitshifted_num1
+        
+       
+        
     return output
 
 if __name__ == "__main__":
     for i in range(1,100):
         for j in range(1,100):
+            print(f"num1: {i}, num2: {j}")
             assert(kertolasku(i,j) == i*j)
